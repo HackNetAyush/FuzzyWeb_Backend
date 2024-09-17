22e69f8ce48c48ce820ec5ebc388942cd04e52b8
HackNetAyush <swami.ayushing1@gmail.com>
2024-09-18 04:22:03 +0530

    ssl testing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { exec } = require("child_process");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
 
 app.post("/scanURL", (req, res) => {
   const { url, scanType } = req.body;
+
+  const filePath = path.join(__dirname, "data.json");
 
   if (scanType === "subdomain") {
     const subdomain_scan = `truncate -s 0 subd.txt && subfinder -d ${url} -o subd.txt`;
@@ -47,7 +50,9 @@ app.post("/scanURL", (req, res) => {
     });
   } else if (scanType === "ssl") {
     console.log("Running SSL Scan");
-    const cmd = `nmap --script ssl-enum-ciphers -p 443 ${url} -oX sslReport.xml  && python xml_to_json.py -x sslReport.xml -o sslReport.json `;
+    const cmd = `nmap --script ssl-enum-ciphers -p 443 ${url} -oX sslReport.xml  && python3 xml_to_json.py -x sslReport.xml -o sslReport.json `;
+
+    const filePath = path.join(__dirname, "sslReport.json");
 
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
@@ -58,21 +63,24 @@ app.post("/scanURL", (req, res) => {
       console.log("stdout: ", stdout);
       console.error("stderr: ", stderr);
 
-      // Reading the file after the subdomain scan completes
-      // fs.readFile("./subd.txt", "utf8", (err, data) => {
-      //   if (err) {
-      //     console.error("Error reading file:", err);
-      //     return res.status(500).send("Error reading file.");
-      //   }
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+          // Handle errors, e.g., file not found or read error
+          res.status(500).send("Error reading file");
+          return;
+        }
 
-      //   // Split the file content into lines
-      //   const lines = data.split("\n").filter((line) => line.trim() !== "");
+        try {
+          // Parse the JSON data
+          const jsonData = JSON.parse(data);
 
-      //   console.log("Lines:", lines); // Array of lines
-      //   res.json({ data: lines });
-      //   // res.send(`File lines: ${lines.join(", ")}`);
-      //   // res.send(`{data: ${lines}}`);
-      // });
+          // Send JSON response
+          res.json(jsonData);
+        } catch (parseError) {
+          // Handle JSON parse errors
+          res.status(500).send("Error parsing JSON");
+        }
+      });
     });
   } else {
     res.status(400).send("Invalid scanType");
