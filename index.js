45f69a89e35641d4979ffda4df25bc65d9f46b5f
HackNetAyush <swami.ayushing1@gmail.com>
2024-09-17 21:40:09 +0530

    Second Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hellooooo");
+});
+
+app.post("/scanURL", (req, res) => {
+  const { url, scanType } = req.body;
+
+  const subdomain_scan = `subfinder -d ${url}`;
+
+  if (scanType === "subdomain") {
+    exec(subdomain_scan, (error, stdout, stderr) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send("Error scanning url");
+      }
+
+      console.log("stdout: ", stdout);
+      console.error("stderr: ", stderr);
+      res.send(`Command output: ${stdout}`);
+    });
+  }
+
+  console.log("URL: ", url);
+  console.log("ScanType: ", scanType);
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
